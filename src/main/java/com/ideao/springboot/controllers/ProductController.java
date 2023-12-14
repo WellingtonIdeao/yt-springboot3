6f575bf924d95040d2b5eb6f3fbdc776f3e754e2
Wellington Ideão <wellington.ideao@gmail.com>
2023-12-14 18:22:17 -0300

    Adicionando Hateoas - hypermidia-links na API

--- src/main/java/com/ideao/springboot/controllers/ProductController.java
+++ src/main/java/com/ideao/springboot/controllers/ProductController.java
@@ -15,13 +15,23 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 public class ProductController {
     @Autowired
     private ProductRepository productRepository;
     @GetMapping("/products")
     public ResponseEntity<List<ProductModel>> getAllProducts(){
-        return ResponseEntity.status(HttpStatus.OK).body(this.productRepository.findAll());
+        List<ProductModel> productsList = this.productRepository.findAll();
+        if(!productsList.isEmpty()){
+            for(ProductModel product: productsList){
+                UUID id = product.getId();
+                product.add(linkTo(methodOn(ProductController.class).getOneProduct(id)).withSelfRel());
+            }
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(productsList);
     }
     @GetMapping("/products/{id}")
     public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id") UUID id){
@@ -29,6 +39,7 @@ public class ProductController {
         if(product0.isEmpty()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
         }
+        product0.get().add(linkTo(methodOn(ProductController.class).getAllProducts()).withRel("Products List"));
         return ResponseEntity.status(HttpStatus.OK).body(product0.get());
     }
     @PostMapping("/products")
